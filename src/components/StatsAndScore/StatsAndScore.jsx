853a434892682f03c6d6866da5557dec448c3bad
maxmay94 <maxmay94@gmail.com>
2022-06-17 18:51:44 -0400

    minor style change to scoreboard

--- src/components/StatsAndScore/StatsAndScore.jsx
+++ src/components/StatsAndScore/StatsAndScore.jsx
@@ -11,10 +11,10 @@ const StatsAndScore = ({score, totalWords, correctGuesses}) => {
       <div className=''>
         {
           correctGuesses &&
-          <ul className='text-black font-light px-5'>
+          <ul className='text-black font-light w-1/3 flex-wrap px-5'>
             {
               correctGuesses?.map((word, i) => (
-                <li key={i} className=''>{word}</li>
+                <li key={i} className='flex border-b border-gray-400 my-1'>{word}</li>
               ))
             }
           </ul>
